10f15167f633f4448ae00d5e52686886c80e50aa
Zack M. Davis <code@zackmdavis.net>
2015-09-27 16:36:12 -0700

    hello redirection world
    Arguably an URL-shortening service should send a 301 rather than a 302,
    but I can imagine it being really annoying in development for your
    browser to permanently cache localhost-port-whatever as whatever until
    you figure out how to intervene. So this service is going to be
    responding 302 for now.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ extern crate iron;
 
 use iron::prelude::*;
 use iron::status;
+use iron::modifiers::Redirect;
+use iron::Url;
 
 
 fn main() {
     Iron::new(|_req: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello, \"World\"!\n")))
+        let destination = Url::parse(
+            "https://github.com/rust-lang/cargo").unwrap();
+        Ok(Response::with((status::Found, Redirect(destination))))
     }).http("localhost:64999").unwrap();
 }
